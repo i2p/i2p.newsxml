6c14ca6c484ea4439dd5e10a3ef61689d3a0f4fc
str4d <str4d@mail.i2p>
2015-06-07 23:02:19 +0000

    Make creating a new entry even easier

--- create_new_entry.sh
+++ create_new_entry.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-sed -i "1i <article\n  id=\"urn:uuid:`uuidgen`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n</p>\n</article>" entries.html
+ENTRIES=entries.html
+
+sed -i "1i <article\n  id=\"urn:uuid:`uuidgen`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n\n</p>\n</article>" $ENTRIES
+
+if [ ! -z "$EDITOR" ]; then
+    case "$EDITOR" in
+        "nano" | "vim")
+            $EDITOR +12 $ENTRIES
+            ;;
+        *)
+            $EDITOR $ENTRIES
+            ;;
+    esac
+fi
