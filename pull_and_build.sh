c848dfaa5e5e04ac449bc14eec991dd036ee879d
str4d <str4d@mail.i2p>
2015-06-14 02:04:25 +0000

    Build SU3 news

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+BUILD=./build
+NEWS_PREFIX=$BUILD/news_
+ATOM_SUFFIX=.atom.xml
+
 tx pull -a
 ./generate_news.py
-# TODO: create SU3
+./make-su3-news.sh $BUILD/news.atom.xml $BUILD/news.su3
+for file in `ls $NEWS_PREFIX*$ATOM_SUFFIX`; do
+    lang=$(expr substr $file $(expr $(expr length $NEWS_PREFIX) + 1) $(expr $(expr length $file) - $(expr length $NEWS_PREFIX) - $(expr length $ATOM_SUFFIX)))
+    ./make-su3-news.sh $file $NEWS_PREFIX$lang.su3
+done
