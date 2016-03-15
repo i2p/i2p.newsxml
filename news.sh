27d979d0322552d66d4b2abddd01d7cd8eaaa811
str4d <str4d@mail.i2p>
2016-03-15 21:25:20 +0000

    Use bulksign method
    Requires i2p.jar built from i2p.i2p 78c0d54b1e21bfed35edf36a695210a8216a5995 on

--- news.sh
+++ news.sh
@@ -3,16 +3,61 @@
 BUILD=./build
 NEWS_PREFIX=$BUILD/news_
 ATOM_SUFFIX=.atom.xml
+TMP=$BUILD/tmp
+TMP_PREFIX=$TMP/news_
+XML_GZ_SUFFIX=.xml.gz
+SU3_SUFFIX=.su3
 
 . ./etc/su3.vars
 [ -f ./etc/su3.vars.custom ] && . ./etc/su3.vars.custom
 
-echo "Enter password for signer $SIGNER in keystore $KS:"
-read password
+verify_xml () {
+    local in="$1"
+
+    # Verify it is valid XML using our parser
+    java -cp $I2P/lib/i2p.jar:$I2P/lib/router.jar:$I2P/lib/routerconsole.jar net.i2p.router.news.NewsXMLParser $in
+    if [ $? -ne 0 ]
+    then
+        echo "XML verification of $in failed"
+        exit 1
+    fi
+    echo
+}
+
+prepare_lang_gz () {
+    local file=$1
+
+    verify_xml $file
+    lang=$(expr substr $file $(expr $(expr length $NEWS_PREFIX) + 1) $(expr $(expr length $file) - $(expr length $NEWS_PREFIX) - $(expr length $ATOM_SUFFIX)))
+    gzip -c -n $file > "$TMP_PREFIX$lang$XML_GZ_SUFFIX"
+}
+
 
 ./generate_news.py
-echo "$password" | ./make-su3-news.sh $BUILD/news.atom.xml $BUILD/news.su3
+
+mkdir -p $TMP
+
+verify_xml $BUILD/news.atom.xml
+gzip -c -n $BUILD/news.atom.xml > "$TMP/news$XML_GZ_SUFFIX"
 for file in `ls $NEWS_PREFIX*$ATOM_SUFFIX`; do
-    lang=$(expr substr $file $(expr $(expr length $NEWS_PREFIX) + 1) $(expr $(expr length $file) - $(expr length $NEWS_PREFIX) - $(expr length $ATOM_SUFFIX)))
-    echo "$password" | ./make-su3-news.sh $file $NEWS_PREFIX$lang.su3
+    prepare_lang_gz $file
 done
+
+NOW=`date +%s`
+java -cp $I2P/lib/i2p.jar net.i2p.crypto.SU3File \
+    bulksign -c NEWS -f XML_GZ -t RSA_SHA512_4096 $TMP $KS $NOW $SIGNER
+
+if [ $? -ne 0 ]
+then
+    echo "Failed to bulksign news files"
+    exit 1
+fi
+
+for file in `ls $TMP_PREFIX*$SU3_SUFFIX`; do
+    java -cp $I2P/lib/i2p.jar net.i2p.crypto.SU3File \
+        showversion $file
+    mv $file $BUILD
+done
+
+rm -r $TMP
+ls -l $BUILD
