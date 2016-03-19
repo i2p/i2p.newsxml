b1356171d4ded3bfeec340b21ccd1daab656de83
zzz <zzz@mail.i2p>
2016-03-19 16:02:23 +0000

    0.9.25

--- create_new_entry.sh
+++ create_new_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ENTRIES=data/entries.html
 
-sed -i "2i <article\n  id=\"urn:uuid:`uuidgen`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n\n</p>\n</article>" $ENTRIES
+sed -i "2i <article\n  id=\"urn:uuid:`uuidgen`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n\n</p>\n</article>\n\n\n" $ENTRIES
 
 if [ ! -z "$EDITOR" ]; then
     case "$EDITOR" in
