696726be4a8b0cfc185f00f1b9f90ba64db4802d
str4d <str4d@mail.i2p>
2016-11-28 11:21:55 +0000

    Try several uuid generation tools

--- create_new_entry.sh
+++ create_new_entry.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ENTRIES=data/entries.html
+UUIDGEN="`which uuidgen || which uuid`"
 
-sed -i "2i <article\n  id=\"urn:uuid:`uuidgen`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n\n</p>\n</article>\n\n\n" $ENTRIES
+sed -i "2i <article\n  id=\"urn:uuid:`$UUIDGEN`\"\n  title=\"\"\n  href=\"\"\n  author=\"\"\n  published=\"\"\n  updated=\"\">\n<details>\n<summary></summary>\n</details>\n<p>\n\n</p>\n</article>\n\n\n" $ENTRIES
 
 if [ ! -z "$EDITOR" ]; then
     case "$EDITOR" in
