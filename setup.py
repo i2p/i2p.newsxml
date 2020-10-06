ff5f2492036f82fec9b64770f224a65feeec8d32
hankhill19580 <hankhill19580@gmail.com>
2020-10-06 04:12:42 +0000

    setup.py didn't work with just one requirement in etc/reqs.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-with open('etc/reqs.txt', 'rb') as infile:
-    install_requires = infile.read().split()
+#This is only needed when reqs.txt has more than one line.
+#with open('etc/reqs.txt', 'rb') as infile:
+#    install_requires = infile.read().split()
 
 setup(
     name='feedgen-i2p',
     description='I2P Atom extension for feedgen',
     author='str4d',
 
-    install_requires=install_requires,
+    install_requires="feedgen==0.3.1",
     packages=['feedgen.ext'],
 )
